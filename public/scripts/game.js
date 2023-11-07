ca9f4a0f0cf6548dd0c4c9f51937219b577d78ad
Allie <allie.hunnius@gmail.com>
2023-11-06 18:11:29 -0800

    Revert "moving squares"
    This reverts commit 3d1bdba596da986529763362bc46f3fba7cc5c0f.

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,84 +1,51 @@
 // game.js
 
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
+const backgroundCanvas = document.getElementById('backgroundCanvas');
+const backgroundCtx = backgroundCanvas.getContext('2d');
 
-// Center the canvas on the screen
-function centerCanvas() {
-    canvas.style.position = 'absolute';
-    canvas.style.left = '50%';
-    canvas.style.top = '50%';
-    canvas.style.transform = 'translate(-50%, -50%)';
-}
+// Draw a white background rectangle on the canvas
+backgroundCtx.fillStyle = 'white';
+backgroundCtx.fillRect(0, 0, backgroundCanvas.width, backgroundCanvas.height);
 
-centerCanvas();
-
-// Define the squares
-const squareWidth = 50;
-const squareHeight = 50;
-const numSquares = 23; // Total number of squares
+const numShapes = 20;
 const animationSpeed = 2;
 
-const circleRadius = 10; // Radius of the circle
-const circleX = 0; // X-coordinate (left edge of the canvas)
-const circleY = canvas.height / 2; // Y-coordinate (middle of the canvas)
-const circleColor = 'black';
-
-function drawCircle() {
-    ctx.fillStyle = circleColor;
-    ctx.beginPath();
-    ctx.arc(circleX, circleY, circleRadius, 0, Math.PI * 2);
-    ctx.fill();
+function randomColor() {
+    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 }
 
-// Function to create a square with a random vertical position and color
-function createSquare(x) {
-    const y = Math.random() * (canvas.height - squareHeight);
-    const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
-    return {
-        x,
-        y,
-        width: squareWidth,
-        height: squareHeight,
+function createShape() {
+    const canvas = document.getElementById('gameCanvas');
+    const ctx = canvas.getContext('2d');
+    
+    const size = Math.random() * 50 + 20;
+    const color = randomColor();
+    const startY = Math.random() * (canvas.height - size);
+    
+    const shape = {
+        x: canvas.width,
+        y: startY,
+        size,
         color,
     };
-}
 
-// Create an array of initial x positions for all squares
-const initialXPositions = Array.from({ length: numSquares }, (_, index) => canvas.width + index * squareWidth);
+    function moveShape() {
+        ctx.fillStyle = shape.color;
+        ctx.fillRect(shape.x, shape.y, shape.size, shape.size);
+        
+        shape.x -= animationSpeed;
 
-// Create an array of squares with random vertical positions and colors
-const squares = initialXPositions.map((x) => createSquare(x));
-
-// Function to move the squares
-function moveSquares() {
-    // Clear the canvas
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Redraw the background
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    // Move and draw the squares
-    squares.forEach((square) => {
-        ctx.fillStyle = square.color; // Use the assigned color
-        ctx.fillRect(square.x, square.y, square.width, square.height);
-        square.x -= animationSpeed;
-
-        // Check if the square has gone off the canvas
-        if (square.x + square.width < 0) {
-            // Reset the square to the initial position
-            square.x = canvas.width;
-            square.y = Math.random() * (canvas.height - squareHeight);
+        if (shape.x + shape.size < 0) {
+            // Shape is out of the canvas, remove it
+            return;
         }
-    });
+        
+        requestAnimationFrame(moveShape);
+    }
 
-    // Request the next frame
-    requestAnimationFrame(moveSquares);
+    requestAnimationFrame(moveShape);
 }
 
-// Start moving the squares
-moveSquares();
-
-// Call the drawCircle() function to draw the circle
-drawCircle();
+for (let i = 0; i < numShapes; i++) {
+    createShape();
+}

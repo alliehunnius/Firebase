43595d8413b5492d8e93bee1e7eaefc497752198
Allie <allie.hunnius@gmail.com>
2023-11-06 22:05:52 -0800

    now the canvas isn't appearing again, i tried restoring the old version, but it didn't seem to work

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,68 +1,84 @@
 // game.js
 
-const backgroundCanvas = document.getElementById('backgroundCanvas');
-const backgroundCtx = backgroundCanvas.getContext('2d');
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
-// Draw a white background rectangle on the canvas
-backgroundCtx.fillStyle = 'white';
-backgroundCtx.fillRect(0, 0, backgroundCanvas.width, backgroundCanvas.height);
-
-const numShapes = 20;
-const animationSpeed = 2;
-
-<<<<<<< Updated upstream
-function randomColor() {
-    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+// Center the canvas on the screen
+function centerCanvas() {
+    canvas.style.position = 'absolute';
+    canvas.style.left = '50%';
+    canvas.style.top = '50%';
+    canvas.style.transform = 'translate(-50%, -50%)';
 }
 
-function createShape() {
-    const canvas = document.getElementById('gameCanvas');
-    const ctx = canvas.getContext('2d');
-    
-    const size = Math.random() * 50 + 20;
-    const color = randomColor();
-    const startY = Math.random() * (canvas.height - size);
-    
-    const shape = {
-        x: canvas.width,
-        y: startY,
-        size,
-=======
+centerCanvas();
+
+// Define the squares
+const squareWidth = 50;
+const squareHeight = 50;
+const numSquares = 23; // Total number of squares
+const animationSpeed = 2;
+
+const circleRadius = 10; // Radius of the circle
+const circleX = 0; // X-coordinate (left edge of the canvas)
+const circleY = canvas.height / 2; // Y-coordinate (middle of the canvas)
+const circleColor = 'black';
+
+function drawCircle() {
+    ctx.fillStyle = circleColor;
+    ctx.beginPath();
+    ctx.arc(circleX, circleY, circleRadius, 0, Math.PI * 2);
+    ctx.fill();
+}
+
 // Function to create a square with a random vertical position and color
 function createSquare(x) {
     const y = Math.random() * (canvas.height - squareHeight);
-    const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256})`;
+    const color = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
     return {
         x,
         y,
         width: squareWidth,
         height: squareHeight,
->>>>>>> Stashed changes
         color,
     };
+}
 
-    function moveShape() {
-        ctx.fillStyle = shape.color;
-        ctx.fillRect(shape.x, shape.y, shape.size, shape.size);
-        
-        shape.x -= animationSpeed;
+// Create an array of initial x positions for all squares
+const initialXPositions = Array.from({ length: numSquares }, (_, index) => canvas.width + index * squareWidth);
 
-        if (shape.x + shape.size < 0) {
-            // Shape is out of the canvas, remove it
-            return;
+// Create an array of squares with random vertical positions and colors
+const squares = initialXPositions.map((x) => createSquare(x));
+
+// Function to move the squares
+function moveSquares() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Redraw the background
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Move and draw the squares
+    squares.forEach((square) => {
+        ctx.fillStyle = square.color; // Use the assigned color
+        ctx.fillRect(square.x, square.y, square.width, square.height);
+        square.x -= animationSpeed;
+
+        // Check if the square has gone off the canvas
+        if (square.x + square.width < 0) {
+            // Reset the square to the initial position
+            square.x = canvas.width;
+            square.y = Math.random() * (canvas.height - squareHeight);
         }
-        
-        requestAnimationFrame(moveShape);
-    }
+    });
 
-    requestAnimationFrame(moveShape);
+    // Request the next frame
+    requestAnimationFrame(moveSquares);
 }
 
-<<<<<<< Updated upstream
-for (let i = 0; i < numShapes; i++) {
-    createShape();
-}
-=======
 // Start moving the squares
 moveSquares();
->>>>>>> Stashed changes
+
+// Call the drawCircle() function to draw the circle
+drawCircle();
